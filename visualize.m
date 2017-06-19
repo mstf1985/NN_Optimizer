0a93761d08ea52057dc7cf0ba66b42945aa01311
Evan <wintrin@qq.com>
2017-06-19 08:30:41 +0800

    update visualize tool

--- visualize.m
+++ visualize.m
@@ -6,17 +6,19 @@ markers = {'--^', '--s', '--o', '--*', '--^', '--s', '--o', '--*', '--^', '--s',
 curve_point = 20;
 for i = 1:length(solvers)
     disp(solvers{i});
-    disp(['./result/', target, '_', solvers{i}, '_train_time.mat']);
     train_time = load(['./result/', target, '_', solvers{i}, '_train_time.mat']);
     train_time = cumsum(train_time.train_time);
-    test_error = load(['./result/', target, '_', solvers{i}, '_test_acc.mat']);
-    test_error = 1 - test_error.test_acc;
-    x = train_time(logical(train_time<=1));
-    y = test_error(logical(train_time<=1));
+    test_acc = load(['./result/', target, '_', solvers{i}, '_test_acc.mat']);
+    [max_acc, max_i] = max(test_acc.test_acc);
+    disp(['acc: ', num2str(max_acc), '; time: ', num2str(train_time(max_i)), '; TLE: ', num2str(max(train_time))]);
+    test_error = 1 - test_acc.test_acc;
+    x = train_time(logical(train_time<=10));
+    y = test_error(logical(train_time<=10));
     index = 1:round(length(x)/curve_point):length(x);
     plot(x(index), y(index), markers{i});
     hold on;
 end
-xlabel('Train Time');
+set(gca, 'YScale', 'log');
+xlabel('Train Time (s)');
 ylabel('Test Error');
 legend(solvers);
